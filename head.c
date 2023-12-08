b476efc3147c4f5bb1a347b9423323c37f4a8539
Takaki Ueno <23036637+uenota@users.noreply.github.com>
2023-12-08 01:20:38 +0000

    read from files

--- head.c
+++ head.c
@@ -3,24 +3,58 @@
 
 static void do_head(FILE *f, long nlines);
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s n\n", argv[0]);
+int main(int argc, char *argv[])
+{
+
+    long nlines;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s n [file file ...]\n", argv[0]);
         exit(1);
     }
-    do_head(stdin, atol(argv[1]));
+
+    nlines = atol(argv[1]);
+    if (argc == 2)
+    {
+        do_head(stdin, nlines);
+    }
+    else
+    {
+        int i;
+
+        for (i = 2; i < argc; i++)
+        {
+            FILE *f;
+            f = fopen(argv[i], "r");
+            if (!f)
+            {
+                perror(argv[i]);
+                exit(1);
+            }
+            do_head(f, nlines);
+            fclose(f);
+        }
+    }
+
     exit(0);
 }
 
-static void do_head(FILE *f, long nlines) {
+static void do_head(FILE *f, long nlines)
+{
     int c;
 
-    if (nlines <= 0) return;
-    while ((c = getc(f) != EOF)) {
-        if (putchar(c) < 0) exit(1);
-        if (c == '\n') {
+    if (nlines <= 0)
+        return;
+    while ((c = getc(f) != EOF))
+    {
+        if (putchar(c) < 0)
+            exit(1);
+        if (c == '\n')
+        {
             nlines--;
-            if (nlines == 0) return;
-        }    
+            if (nlines == 0)
+                return;
+        }
     }
 }
